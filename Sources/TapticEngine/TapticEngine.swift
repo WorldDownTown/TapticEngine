63800a6970a933719b23af93ed2335d31c4840be
iAugus <iAugux@gmail.com>
2018-12-19 17:44:24 +0800

    Updated to Swift4.2 syntax

--- Sources/TapticEngine/TapticEngine.swift
+++ Sources/TapticEngine/TapticEngine.swift
@@ -32,7 +32,7 @@ open class TapticEngine {
         private static func makeGenerator(_ style: ImpactStyle) -> Any? {
             guard #available(iOS 10.0, *) else { return nil }
 
-            let feedbackStyle: UIImpactFeedbackStyle
+            let feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle
             switch style {
             case .light:
                 feedbackStyle = .light
@@ -126,7 +126,7 @@ open class TapticEngine {
             guard #available(iOS 10.0, *),
                 let generator = generator as? UINotificationFeedbackGenerator else { return }
 
-            let feedbackType: UINotificationFeedbackType
+            let feedbackType: UINotificationFeedbackGenerator.FeedbackType
             switch type {
             case .success:
                 feedbackType = .success
